0b6b959f2634110c996c7178880a0fbba7c7a77b
Steen Clarke <cmclarkey@gmail.com>
2016-08-14 09:58:59 -0700

    Add contact us page and signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'index', to: 'portfolios#index'
+  get '/', to: 'portfolios#index'
+  get '/index/', to: 'portfolios#index'
+  get '/signup/', to: 'portfolios#signup'
+  get '/contact/', to: 'portfolios#contact'
 end
